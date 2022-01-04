5a3beec00ee36b98c7900a89f695124c742306bf
Olha <95411084+oleris@users.noreply.github.com>
2022-01-04 20:59:38 +0200

    Add files via upload

---
+++ T4/T4/T4/T4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Client.h"
+
+int main()
+{
+    Item* item = new Item("Bicycle", 100);
+    Shop* shop = new Shop();
+    Client* client = new Client();
+    client->items.push_back(item);
+    client->SetMoney(1000);
+    std::cout << client->GetMoney();
+    client->PlaceOrder(shop);
+    client->PlaceOrder(shop);
+   std::cout << client->GetMoney();
+
+}
+
+
